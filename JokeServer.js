1df07e01b99be2c83422bc4a26f0b26c98a30afe
Jeremy Chambers <jchambers@cars.com>
2015-01-16 12:54:26 -0600

    Added admin and client listeners to JokeServer

--- JokeServer.js
+++ JokeServer.js
@@ -1,3 +1,16 @@
-/**
- * Created by jeremychambers on 1/16/15.
- */
+var http = require('http');
+
+http.createServer(clientListener).listen(4502);
+http.createServer(adminListener).listen(9506);
+
+var clientInfo = {};
+
+function clientListener(req, resp) {
+    resp.write("Client Request");
+    resp.end();
+}
+
+function adminListener(req, resp) {
+    resp.write("Admin Request");
+    resp.end();
+}
