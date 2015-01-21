a008ce61908a6a08150a8e56f5ba158c1951822b
Jeremy "TheHipbot" Chambers <jeremy@thehipbot.com>
2015-01-20 23:25:54 -0600

    npm init, pulled in socket.io, created socket listeners, created admin and client html files, wrote files out to client

--- JokeServer.js
+++ JokeServer.js
@@ -1,16 +1,61 @@
 var http = require('http');
+var fs = require('fs');
+var io = require('socket.io');
 
-http.createServer(clientListener).listen(4502);
-http.createServer(adminListener).listen(9506);
+// create http servers for ports to serve up client side code
+var client = http.createServer(clientListener).listen(4502, function () {
+    console.log("Client Listening on port 4502\n");
+});
 
-var clientInfo = {};
+var admin = http.createServer(adminListener).listen(9506,  function () {
+    console.log("Admin Listening on port 9506");
+});
 
-function clientListener(req, resp) {
-    resp.write("Client Request");
-    resp.end();
+// create admin and client sockets
+var ioClient = io.listen(client);
+var ioAdmin = io.listen(admin);
+
+// client info object
+var serverInfo = {
+    state: 1,
+    clients: {}
+};
+
+ioClient.on('connection', function (socket) {
+    socket.on('request', function (data) {
+        if (!serverInfo.clients[data]) {
+            serverInfo.clients[data] = {};
+            socket.emit('get name');
+        } else {
+
+        }
+    });
+
+    socket.on('set name', function (data) {
+        serverInfo.clients[data.id] = {
+            name: data.name,
+            jokes: [false, false, false, false, false],
+            proverbs: [false, false, false, false, false]
+        }
+    });
+});
+
+ioAdmin.on('connection', function (socket) {
+    socket.on();
+});
+
+function clientListener (req, resp) {
+    if (req.url !== '/favicon.ico') {
+        // console logging
+    }
+    resp.writeHead(200);
+    fs.createReadStream(__dirname + "/client.html").pipe(resp);
 }
 
-function adminListener(req, resp) {
-    resp.write("Admin Request");
-    resp.end();
+function adminListener (req, resp) {
+    if (req.url !== '/favicon.ico') {
+        // console logging
+    }
+    resp.writeHead(200);
+    fs.createReadStream(__dirname + "/admin.html").pipe(resp);
 }
