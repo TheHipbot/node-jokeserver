6c192b043f89d1d11fc69d930e7453e692b4782b
Jeremy "TheHipbot" Chambers <jeremy@thehipbot.com>
2015-01-21 19:41:09 -0600

    Changed state to hold string, added uuid, added jokes, randomized

--- JokeServer.js
+++ JokeServer.js
@@ -17,17 +17,56 @@ var ioAdmin = io.listen(admin);
 
 // client info object
 var serverInfo = {
-    state: 1,
+    state: 'jokes',
     clients: {}
 };
+
+var jokes = [
+    "How many programmers does it take to change a light bulb? none, that's a hardware problem\n",
+    "A neutron walks into a bar and asks how much for a beer? The bartender says, for you? no charge.\n",
+    "A man didn't like his haircut, but it started to grow on him.\n",
+    "What do you call a bear with no teeth? A gummy bear.\n",
+    "What's the best way to carve wood? Whittle by whittle.\n"
+];
+
+var proverbs = [
+    "A penny saved is a penny earned\n",
+    "A bird in the hand is worth two in the bush\n",
+    "A drowning man will clutch at a straw\n",
+    "A journey of a thousand miles begins with a single step\n",
+    "A poor workman always blames his tools\n"
+];
 
 ioClient.on('connection', function (socket) {
     socket.on('request', function (data) {
         if (!serverInfo.clients[data.id]) {
             socket.emit('get name');
         } else {
-            socket.emit('response', { message: "test" });
-            console.log("Response sent to " + serverInfo.clients[data.id].name);
+            var message,
+                r = Math.floor(Math.random() * 5),
+                allUsed = true;
+
+            if (serverInfo.state === 'maintenance') {
+                socket.emit('response', { message: "Server Maintenance, please try again later" });
+            } else {
+
+                for (var i = 0; i < 5; i++) {
+                    allUsed = allUsed && serverInfo.clients[data.id][serverInfo.state][i];
+                }
+
+                if (allUsed) {
+                    serverInfo.clients[data.id][serverInfo.state] = [false, false, false, false, false];
+                }
+
+                while (serverInfo.clients[data.id][serverInfo.state][r]) {
+                    r = Math.floor(Math.random() * 5);
+                }
+
+                message = (serverInfo.state === 'jokes') ?  jokes[r] : proverbs[r];
+
+                socket.emit('response', {message: message});
+                serverInfo.clients[data.id][serverInfo.state][r] = true;
+            }
         }
     });
 
@@ -47,7 +86,7 @@ ioAdmin.on('connection', function (socket) {
 
     socket.on('change state', function (data) {
         serverInfo.state = data.state;
-        socket.emit('state', { state: serverInfo.state });
+        ioAdmin.sockets.emit('state', { state: serverInfo.state });
         console.log("State change to " + serverInfo.state);
     });
 
